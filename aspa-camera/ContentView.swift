d434b127909679b36bbafa246e61182ed6426917
takatronix <takatronix@gmail.com>
2026-02-10 08:33:58 +0900

    Initial Commit

---
+++ aspa-camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aspa-camera
+//
+//  Created by Takashi Otsuka on 2026/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aspa-camera/aspa_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  aspa_cameraApp.swift
+//  aspa-camera
+//
+//  Created by Takashi Otsuka on 2026/02/10.
+//
+
+import SwiftUI
+
+@main
+struct aspa_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
